30de09f062680036fd0caacd4845e1b64102b4f7
Kian <macamos.kian@gmail.com>
2025-03-02 12:42:08 +0800

    RemasteredV4

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Typed.js
   const typed = new Typed(".autoType", {
     strings: ["DEVELOPER", "PROGRAMMER"],
     typeSpeed: 40,
@@ -12,8 +13,22 @@ document.addEventListener("DOMContentLoaded", function () {
   const menuToggle = document.querySelector(".menu-toggle");
   const navLinks = document.querySelector("nav ul");
 
-  // Toggle the navigation links on hamburger icon click
   menuToggle.addEventListener("click", () => {
     navLinks.classList.toggle("show");
   });
+  // Contact form
+  document
+    .getElementById("contactForm")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      const name = document.getElementById("fullname").value;
+      const email = document.getElementById("email").value;
+      const message = document.getElementById("message").value;
+
+      const mailtoLink = `mailto:${email}?subject=Contact from ${name}&body=${encodeURIComponent(
+        message
+      )}`;
+      window.location.href = mailtoLink;
+    });
 });
